37d04f0be720adb9e3239f3c2c5d8e40be4efb91
pawlo4-94 <pawlo4-94@o2.pl>
2021-07-23 20:00:14 +0200

    index.js in src edited

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Tasks from './features/tasks/Tasks';
+import { App } from "./App";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Tasks />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
