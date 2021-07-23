bcd80401ebe0f3cd1722e5cddf3de7aca7984193
pawlo4-94 <pawlo4-94@o2.pl>
2021-07-23 21:00:24 +0200

    useSelector added

--- src/features/tasks/TaskList/index.js
+++ src/features/tasks/TaskList/index.js
@@ -1,27 +1,32 @@
+import { useSelector } from "react-redux";
+import { selectTasks } from "../tasksSlice";
 import { StyledTasks, StyledItem, StyledSpan, StyledButton } from "./styled";
 
-const TaskList = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
-    <StyledTasks>
-        {tasks.map(task => (
-            <StyledItem hidden={hideDone && task.done}
-                key={task.id}
-            >
-                <StyledButton done
-                    onClick={() => toggleTaskDone(task.id)}
+const TaskList = ({ removeTask, toggleTaskDone }) => {
+    const { tasks, hideDone } = useSelector(selectTasks);
+    return (
+        <StyledTasks>
+            {tasks.map(task => (
+                <StyledItem hidden={hideDone && task.done}
+                    key={task.id}
                 >
-                    {task.done ? "✔" : ""}
-                </StyledButton>
-                <StyledSpan done={task.done}>
-                    {task.content}
-                </StyledSpan>
-                <StyledButton remove
-                    onClick={() => removeTask(task.id)}
-                >
-                    🗑
-                </StyledButton>
-            </StyledItem>
-        ))}
-    </StyledTasks>
-);
+                    <StyledButton done
+                        onClick={() => toggleTaskDone(task.id)}
+                    >
+                        {task.done ? "✔" : ""}
+                    </StyledButton>
+                    <StyledSpan done={task.done}>
+                        {task.content}
+                    </StyledSpan>
+                    <StyledButton remove
+                        onClick={() => removeTask(task.id)}
+                    >
+                        🗑
+                    </StyledButton>
+                </StyledItem>
+            ))}
+        </StyledTasks>
+    )
+};
 
 export default TaskList;
