51e64483c8662b8366d18446ee219d4c2ce54fbb
pawlo4-94 <pawlo4-94@o2.pl>
2021-07-25 22:36:24 +0200

    useTasks hook deleted from Tasks.js

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -4,42 +4,21 @@ import Buttons from './Buttons';
 import Section from '../../common/Section';
 import Header from "../../common/Header";
 import Container from "../../common/Container";
-import { useTasks } from '../../useTasks';
 import { theme } from '../../common/theme.js';
 import { ThemeProvider } from 'styled-components';
 
 function Tasks() {
-
-  const {
-    // tasks,
-    removeTask,
-    toggleTaskDone,
-    setAllDone,
-    addNewTask
-  } = useTasks();
-
   return (
     <ThemeProvider theme={theme}>
       <Container>
         <Header title="Lista Zadań" />
         <Section
           title="Dodaj nowe zadanie"
-          body={<Form
-            addNewTask={addNewTask}
-          />}
+          body={<Form />}
         />
         <Section title="Lista zadań"
-          body={
-            <TaskList
-              removeTask={removeTask}
-              toggleTaskDone={toggleTaskDone}
-            />
-          }
-          extraHeaderContent={
-            <Buttons
-              setAllDone={setAllDone}
-            />
-          }
+          body={<TaskList />}
+          extraHeaderContent={<Buttons />}
         />
       </Container>
     </ThemeProvider>
