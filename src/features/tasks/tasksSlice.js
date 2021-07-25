b5cf0e2b58d9991beb27617219ca05ae4801eccb
pawlo4-94 <pawlo4-94@o2.pl>
2021-07-25 13:58:13 +0200

    toggleTaskDone and removeTask reducer added

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -12,10 +12,18 @@ const tasksSlice = createSlice({
         },
         toggleHideDone: state => {
             state.hideDone = !state.hideDone;
+        },
+        toggleTaskDone: (state, action) => {
+            const index = state.tasks.findIndex(task => task.id === action.payload);
+            state.tasks[index].done = !state.tasks[index].done;
+        },
+        removeTask: (state, action) => {
+            const index = state.tasks.findIndex(task => task.id === action.payload);
+            state.tasks.splice(index, index + 1);
         }
     },
 });
 
-export const { addTask, toggleHideDone } = tasksSlice.actions;
+export const { addTask, toggleHideDone, toggleTaskDone, removeTask } = tasksSlice.actions;
 export const selectTasks = state => state.tasks;
 export default tasksSlice.reducer;
