57812e956b4cb433afb68b0a88316eb601a0213d
pawlo4-94 <pawlo4-94@o2.pl>
2021-08-13 22:26:19 +0200

    ThemeProvider in App.js only

--- src/App.js
+++ src/App.js
@@ -3,32 +3,36 @@ import AuthorPage from "./features/author/AuthorPage";
 import TaskPage from "./features/tasks/TaskPage/index";
 import { StyledNavLink, StyledNavigation } from "./styled";
 import { Switch, Route, HashRouter, Redirect } from 'react-router-dom';
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
 
 export const App = () => (
-    <HashRouter>
-        <nav>
-            <StyledNavigation>
-                <li>
-                    <StyledNavLink to="/zadania">Zadania</StyledNavLink>
-                </li>
-                <li>
-                    <StyledNavLink to="/autor">O autorze</StyledNavLink>
-                </li>
-            </StyledNavigation>
-        </nav>
-        <Switch>
-            <Route path="/zadania/:id">
-                <TaskPage />
-            </Route>
-            <Route path="/zadania">
-                <TasksPage />
-            </Route>
-            <Route path="/autor">
-                <AuthorPage />
-            </Route>
-            <Route path="/">
-                <Redirect to="/zadania" />
-            </Route>
-        </Switch>
-    </HashRouter>
+    <ThemeProvider theme={theme}>
+        <HashRouter>
+            <nav>
+                <StyledNavigation>
+                    <li>
+                        <StyledNavLink to="/zadania">Zadania</StyledNavLink>
+                    </li>
+                    <li>
+                        <StyledNavLink to="/autor">O autorze</StyledNavLink>
+                    </li>
+                </StyledNavigation>
+            </nav>
+            <Switch>
+                <Route path="/zadania/:id">
+                    <TaskPage />
+                </Route>
+                <Route path="/zadania">
+                    <TasksPage />
+                </Route>
+                <Route path="/autor">
+                    <AuthorPage />
+                </Route>
+                <Route path="/">
+                    <Redirect to="/zadania" />
+                </Route>
+            </Switch>
+        </HashRouter>
+    </ThemeProvider>
 );
