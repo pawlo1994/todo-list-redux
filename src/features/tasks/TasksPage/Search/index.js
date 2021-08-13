fcabf04c459ef44b6b8056fac8f6d9d039c3c0c6
pawlo4-94 <pawlo4-94@o2.pl>
2021-08-13 22:27:39 +0200

    useReplaceQueryParameter added

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,25 +1,17 @@
-import { useHistory, useLocation } from "react-router-dom";
 import { StyledInput } from "../../../tasks/Input/index";
 import { Wrapper } from "./styled";
 import { searchQueryParamName } from "../searchQueryParamName";
-import { useQueryParameter } from "./queryParameters";
+import { useQueryParameter, useReplaceQueryParameter } from "./queryParameters";
 
 export const Search = () => {
-    const location = useLocation();
-    const history = useHistory();
+    const replaceQueryParameter = useReplaceQueryParameter();
     const query = useQueryParameter(searchQueryParamName);
 
     const onInputChange = ({ target }) => {
-        const searchParams = new URLSearchParams(location.search);
-
-        if (target.value.trim() === "") {
-            searchParams.delete(searchQueryParamName);
-        }
-        else {
-            searchParams.set(searchQueryParamName, target.value);
-        }
-
-        history.push(`${location.pathname}?${searchParams.toString()}`)
+        replaceQueryParameter({
+            key: searchQueryParamName,
+            value: target.value.trim() !== "" ? target.value : "",
+        });
     };
 
     return (
--- src/features/tasks/TasksPage/Search/queryParameters.js
+++ src/features/tasks/TasksPage/Search/queryParameters.js
@@ -1,6 +1,23 @@
-import { useLocation } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 export const useQueryParameter = searchQueryParamName => {
     const location = useLocation();
     return (new URLSearchParams(location.search)).get(searchQueryParamName);
 };
+
+export const useReplaceQueryParameter = () => {
+    const location = useLocation();
+    const history = useHistory();
+    const replaceQueryParameter = ({ key, value }) => {
+        const searchParams = new URLSearchParams(location.search);
+        if (value === "") {
+            searchParams.delete(key);
+        }
+        else {
+            searchParams.set(key, value);
+        }
+
+        history.push(`${location.pathname}?${searchParams.toString()}`);
+    }
+    return replaceQueryParameter;
+}
