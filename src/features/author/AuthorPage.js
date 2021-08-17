196dcaa184d7a5ce1ee75b056e473333a20e3ea4
pawlo4-94 <pawlo4-94@o2.pl>
2021-08-17 08:56:29 +0200

    formatting in AuthorPage fixed

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -12,24 +12,32 @@ function AuthorPage() {
                 title="Paweł Suchy"
                 body={
                     <>
-                        <StyledParagraph>Nazywam się <strong>Paweł Suchy</strong>, mam 26 lat, mieszkam w
+                        <StyledParagraph>
+                            Nazywam się <strong>Paweł Suchy</strong>, mam 26 lat, mieszkam w
                             Międzyrzeczu
-                            Dolnym koło Bielska-Białej.</StyledParagraph>
-                        <StyledParagraph>Ukończyłem technikum informatyczne w Zespole Szkół "Silesia" w
-                            Czechowicach-Dziedzicach jednak póki co nie pracuję w zawodzie.</StyledParagraph>
-                        <StyledParagraph>Aktualnie pracuję na produkcji w branży Automotive jednak bardzo mocno
+                            Dolnym koło Bielska-Białej.
+                        </StyledParagraph>
+                        <StyledParagraph>
+                            Ukończyłem technikum informatyczne w Zespole Szkół "Silesia" w
+                            Czechowicach-Dziedzicach jednak póki co nie pracuję w zawodzie.
+                        </StyledParagraph>
+                        <StyledParagraph>
+                            Aktualnie pracuję na produkcji w branży Automotive jednak bardzo mocno
                             wierzę w to,
                             że po ukończeniu tego kursu uda mi się, jeśli nie zmienić pracę na stałe, to chociaż zacząć dorabiać
-                            na frontendzie.</StyledParagraph>
-                        <StyledParagraph>Poza pracą od prawie 13 lat gram na puzonie a od prawie 12 w Młodzieżowej
+                            na frontendzie.
+                        </StyledParagraph>
+                        <StyledParagraph>
+                            Poza pracą od prawie 13 lat gram na puzonie a od prawie 12 w Młodzieżowej
                             Orkiestrze Dętej Miejskiego Domu Kultury w Czechowicach-Dziedzicach. Poza tą orkiestrą miałem
                             również okazję wziąć udział w czterech edycjach Żywieckiego Suwakowania a także zagrać dwa koncerty
-                            razem z zespołem Golec uOrkiestra.</StyledParagraph>
+                            razem z zespołem Golec uOrkiestra.
+                        </StyledParagraph>
                     </>
                 }
             />
         </Container>
     );
-}
+};
 
 export default AuthorPage;
